b4df00ba959a7a7f9bbe6959cd14a616bcebccab
Marisa-Chan <thunder_8888@mail.ru>
2016-11-09 13:34:00 +0700

    For 2D graphics now used linear filtering

--- src/wrapSDL.cpp
+++ src/wrapSDL.cpp
@@ -105,8 +105,8 @@ void SDLWRAP_recreateSurface()
 
     glBindTexture(GL_TEXTURE_2D, screenTex);
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, screen->w, screen->h, 0, pixfmt, pixtype, NULL);
 
